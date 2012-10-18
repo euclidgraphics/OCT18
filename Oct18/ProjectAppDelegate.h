4136108bf1b801b16b6ca6c72a11868ea06a2ee1
elizabeth mcalpin <sbr341@gmail.com>
2012-10-18 09:43:26 -0400

    copy files

---
+++ Oct18/ProjectAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ProjectAppDelegate.h
+//  Oct18
+//
+//  Created by Elizabeth  McAlpin on 10/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
