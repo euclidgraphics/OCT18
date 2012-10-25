3362865fa6d6f724a9728fd2225ff34f45770534
elizabeth mcalpin <sbr341@gmail.com>
2012-10-24 20:59:19 -0400

    first commit

---
+++ Oct24/Oct24/InterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  InterAppDelegate.h
+//  Oct24
+//
+//  Created by Elizabeth  McAlpin on 10/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Oct24/es.lproj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Oct24
+//
+//  Created by Elizabeth  McAlpin on 10/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "InterAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([InterAppDelegate class]));
+    }
+}
